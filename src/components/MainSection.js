f82de5c55dbad4379a374233de63cfabdeac6d18
vrlcnnDeneme <bilimtadinda@gmail.com>
2023-01-29 14:07:24 +0300

    add margin to x axis

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -4,7 +4,7 @@ const MainSection = () => {
 	return <div>
 		<div className='flex justify-center items-center'>
 			<hr className="w-32 h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
-			MainSection
+			<h2 className='mx-4'>Ürünlerimiz</h2>
 			<hr className="w-32 h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
 		</div>
 	</div>
