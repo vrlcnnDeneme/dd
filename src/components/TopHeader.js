902867bbfc5f2598003c0079ded5fb2a9a214def
vrlcnnDeneme <bilimtadinda@gmail.com>
2023-01-29 13:18:51 +0300

    add topheader to padding

--- src/components/TopHeader.js
+++ src/components/TopHeader.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const TopHeader = () => {
-	return <div className='hidden md:block py-2.5'>
+	return <div className='hidden md:block py-3' style={{ color: '#2250f4' }}>
 		<span className="float-left">
 			<ul className='list-none pl-3'>
 				<li className='inline-block pl-3'>
@@ -14,10 +14,17 @@ const TopHeader = () => {
 
 		</span>
 		<span className="float-right">
-			<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-				<path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
-			</svg>
-            0537 936 ** **
+			<ul className='list-none pr-3'>
+				<li className='inline-block pr-2'>
+					<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+						<path strokeLinecap="round" strokeLinejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 002.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 01-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 00-1.091-.852H4.5A2.25 2.25 0 002.25 4.5v2.25z" />
+					</svg>
+				</li>
+				<li className='inline-block pr-3'>
+					0537 936 ** **
+				</li>
+			</ul>
+
 		</span>
 	</div>
 }
