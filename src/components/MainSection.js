40a2cc8e33323c7dabd209cb0c0bffce7437afcf
vrlcnnDeneme <bilimtadinda@gmail.com>
2023-01-29 14:43:00 +0300

    add slider

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -1,4 +1,25 @@
 import React from 'react'
+import Carousel from 'react-multi-carousel'
+import 'react-multi-carousel/lib/styles.css'
+const responsive = {
+	superLargeDesktop: {
+		// the naming can be any, depends on you.
+		breakpoint: { max: 4000, min: 3000 },
+		items: 5
+	},
+	desktop: {
+		breakpoint: { max: 3000, min: 1024 },
+		items: 3
+	},
+	tablet: {
+		breakpoint: { max: 1024, min: 464 },
+		items: 2
+	},
+	mobile: {
+		breakpoint: { max: 464, min: 0 },
+		items: 1
+	}
+}
 
 const MainSection = () => {
 	return <div>
@@ -15,6 +36,12 @@ const MainSection = () => {
 				Daha mutlu , daha enerjik, daha sağlıklı olmayı hak ediyoruz. Bu konuda yaşam kalitenizi destekleyecek ürünlerimiz ile tanışın.
 			</p>
 		</div>
+		<Carousel responsive={responsive}>
+			<div>Item 1</div>
+			<div>Item 2</div>
+			<div>Item 3</div>
+			<div>Item 4</div>
+		</Carousel>;
 	</div>
 }
 
