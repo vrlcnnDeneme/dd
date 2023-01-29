fc68ccec5efc457bac582dc8b422e6a4b4c13abf
vrlcnnDeneme <bilimtadinda@gmail.com>
2023-01-29 11:30:24 +0300

    from change title

--- src/App.js
+++ src/App.js
@@ -15,7 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         
+         
+          Learn React with vural
 
         </a>
       </header>
