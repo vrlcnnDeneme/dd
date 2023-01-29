84d8b6f6b820f4d1a9fe195c7f67f85a2333a226
vrlcnnDeneme <bilimtadinda@gmail.com>
2023-01-29 14:18:20 +0300

    add headin

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -4,7 +4,7 @@ const MainSection = () => {
 	return <div>
 		<div className='flex justify-center items-center'>
 			<hr className="w-32 h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
-			<h2 className='mx-5 text-4xl font-bold'>Ürünlerimiz</h2>
+			<h2 className='mx-5 text-3xl font-bold'>Ürünlerimiz</h2>
 			<hr className="w-32 h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
 		</div>
 	</div>
