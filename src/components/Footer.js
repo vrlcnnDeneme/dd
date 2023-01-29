08decae53e9f036456f5dc566ec57343551f2aaf
vrlcnnDeneme <bilimtadinda@gmail.com>
2023-01-29 18:10:45 +0300

    add ssr to slider

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,15 @@ import React from 'react'
 
 const Footer = () => {
 	<div className='my-24'>
-		<div>Logo</div>
+		<div>
+			Logo
+			<hr className="h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
+		</div>
 		<div>Bilgiler</div>
-		<div>Copyright © 2023 site adi All rights reserved.</div>
+		<div className='mt-6'>
+			<hr className="w-32 h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
+			Copyright © 2023 site adi All rights reserved.
+		</div>
 	</div>
 }
 
