a0afbb7b75bada39fd0e8de9a08bcd2c9655a679
vrlcnnDeneme <bilimtadinda@gmail.com>
2023-01-29 14:04:00 +0300

    content center1

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const MainSection = () => {
 	return <div>
-		<div className='flex items-center'>
+		<div className='flex content-center items-center'>
 			<hr className="w-32 h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
 			MainSection
 			<hr className="w-32 h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
