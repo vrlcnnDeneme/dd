1ff90e8d51a05b6e3cb7e673d6b0a12d62f9172e
vrlcnnDeneme <bilimtadinda@gmail.com>
2023-01-29 13:46:48 +0300

    add radius

--- src/components/MainHeader.js
+++ src/components/MainHeader.js
@@ -14,13 +14,13 @@ const MainHeader = () => {
 					<li className='inline-block pl-2'>İletişim</li>
 				</ul>
 			</div>
-			<div className='md:hidden p-2'>
-				<button style={{ backgroundColor: '#2250f4', color: '#ffffff' }}>Kayıt Ol</button>
+			<div className='md:hidden p-2 pr-5'>
+				<button className='rounded-xl' style={{ backgroundColor: '#2250f4', color: '#ffffff' }}>Kayıt Ol</button>
 			</div>
 			<div className='hidden md:block p-2'>
-				<button style={{ backgroundColor: '#2250f4', color: '#ffffff' }}>Kayıt Ol</button>
+				<button className='rounded-xl' style={{ backgroundColor: '#2250f4', color: '#ffffff' }}>Kayıt Ol</button>
 			</div>
-			<div className='md:hidden pr-5'>
+			<div className='md:hidden'>
 				<svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" />
 			</div>
 
