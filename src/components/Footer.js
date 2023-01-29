2bb682eba526532e6a2099ea5f11b946694776a8
vrlcnnDeneme <bilimtadinda@gmail.com>
2023-01-29 18:15:52 +0300

    add ssr to slider

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,14 @@ const Footer = () => {
 			Logo
 			<hr className="h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
 		</div>
-		<div>Bilgiler</div>
+		<div className="flex justify-between">
+			<div>
+				İletşime Geçin
+			</div>
+			<div>
+				Linklerimiz
+			</div>
+		</div>
 		<div className='mt-6'>
 			<hr className="w-32 h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
 			Copyright © 2023 site adi All rights reserved.
