4877959ff9f5863f2d2667143d0aa1dfaa69626c
vrlcnnDeneme <bilimtadinda@gmail.com>
2023-01-29 18:18:24 +0300

    add ssr to slider

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,21 +2,23 @@ import React from 'react'
 
 const Footer = () => {
 	return <div className='my-24'>
-		<div>
+		<div className='container mx-auto max-w-screen-lg px-5'>
+			<div>
 			Logo
-			<hr className="h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
-		</div>
-		<div className="flex justify-between">
-			<div>
+				<hr className="h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
+			</div>
+			<div className="flex justify-between">
+				<div>
 				İletşime Geçin
-			</div>
-			<div>
+				</div>
+				<div>
 				Linklerimiz
+				</div>
 			</div>
-		</div>
-		<div className='mt-6'>
-			<hr className="w-32 h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
+			<div className='mt-6'>
+				<hr className="w-32 h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
 			Copyright © 2023 site adi All rights reserved.
+			</div>
 		</div>
 	</div>
 }
