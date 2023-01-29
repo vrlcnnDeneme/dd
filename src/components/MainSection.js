40bb9956011ed759609340c540932afb7f30d278
vrlcnnDeneme <bilimtadinda@gmail.com>
2023-01-29 14:01:35 +0300

    align itemss on mainheader

--- src/components/MainSection.js
+++ src/components/MainSection.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const MainSection = () => {
 	return <div>
-        MainSection <hr className="w-64 h-1 my-8 bg-gray-200 border-0 rounded dark:bg-gray-700" />
+		<div className='text-center'>
+			<hr className="w-32 h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
+			MainSection
+			<hr className="w-32 h-1 my-8 bg-gray-200 border-0 rounded" style={{ backgroundColor: '#2250f4' }} />
+		</div>
 	</div>
 }
 
